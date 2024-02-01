1be6a2a7903b6c08a16c512009615c52f4cd72a1
Preetam Sahu <preetamsahu1290@gmail.com>
2024-02-01 16:05:45 +0530

    success

--- js/values.js
+++ js/values.js
@@ -64,8 +64,27 @@ var isprevalue = [[
     [true, false, false, false, true, false, false, true, false],
     [false, false, false, true, false, false, true, false, false]
 ],
-[true, false, false, false, true, false, false, true, false, false, false, true, true, false, false, false, false, true, false, true, false, false, false, false, false, false, true, false, false, false, false, false, true, false, false, true, false, true, true, false, false, false, true, true, false, true, false, false, true, false, false, false, false, false, true, false, false, false, false, false, false, true, false, true, false, false, false, false, true, true, false, false, false, true, false, false, true, false, false, false, true],
-[false, false, false, true, true, true, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, true, true, false, false, false, false, false, false, true, false, false, true, true, false, false, false, false, true, false, false, false, false, false, true, false, false, false, true, false, false, true, false, false, false, false, false, false, false, false]
+[
+    [true, false, false, false, true, false, false, true, false],
+    [false, false, true, true, false, false, false, false, true],
+    [false, true, false, false, false, false, false, false, true],
+    [false, false, false, false, false, true, false, false, true],
+    [false, true, true, false, false, false, true, true, false],
+    [true, false, false, true, false, false, false, false, false],
+    [true, false, false, false, false, false, false, true, false],
+    [true, false, false, false, false, true, true, false, false]
+],
+
+[
+    [false, false, false, true, true, true, false, false, false],
+    [false, false, true, false, false, false, false, false, false],
+    [false, true, false, false, false, false, true, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, true, false, false, false, false, true, true, false],
+    [false, false, false, false, false, true, false, false, true],
+    [true, false, false, false, false, true, false, false, false],
+    [false, false, true, false, false, false, true, false, false]
+]
 ]
 
 console.log(isprevalue.length)
